aef067110a29f452405fb7014451e6c0ccbcfafe
evanbowman <ebowman@bu.edu>
2016-06-12 13:59:08 -0400

    TeleporterSmoke derives from Effect base class

--- src/teleporterSmoke.cpp
+++ src/teleporterSmoke.cpp
@@ -10,27 +10,17 @@
 
 #define ANIMATION_LENGTH 6
 
-TeleporterSmoke::TeleporterSmoke(sf::Texture * pTxtr, float x, float y) {
-	xPos = x;
-	xInit = x;
-	yPos = y;
-	yInit = y;
+TeleporterSmoke::TeleporterSmoke(sf::Texture * pTxtr, float x, float y) : Effect(x, y) {
 	spriteSheet.setTexture(pTxtr);
-	// Initialize the current frame of the animation to the first one
-	frameIndex = 0;
-	frameTimer = 0;
-	killFlag = false;
-	// Initialize the depth to 0
 	drawOrder = 0;
 }
 
-void TeleporterSmoke::update(float xOffset, float yOffset, sf::Time & elapsedTime) {
-	xPos = xInit + xOffset;
+void TeleporterSmoke::update(float xOffset, float yOffset, const sf::Time & elapsedTime) {
 	yPos = yInit + yOffset;
 	// Drift up
 	yInit -= (elapsedTime.asMilliseconds() / 17.6) * 0.6;
-	frameTimer += elapsedTime.asMilliseconds();
-	spriteSheet.setPosition(xPos, yPos);
+	timer += elapsedTime.asMilliseconds();
+	spriteSheet.setPosition(xInit + xOffset, yPos);
 }
 
 const sf::Sprite & TeleporterSmoke::getSprite() {
@@ -38,8 +28,8 @@ const sf::Sprite & TeleporterSmoke::getSprite() {
 	// to happen here. This is a special case where the sprite maps to a different
 	// vector depending on its layer. In other instances, 'get...()' member functions
 	// should not alter internal datafields for objects.
-	if (frameTimer > 105) {
-		frameTimer -= 105;
+	if (timer > 105) {
+		timer -= 105;
 		frameIndex++;
 		if (frameIndex > 2) {
 			drawOrder = 1;
@@ -51,18 +41,6 @@ const sf::Sprite & TeleporterSmoke::getSprite() {
 		}
 	}
 	return spriteSheet[frameIndex];
-}
-
-bool TeleporterSmoke::getKillFlag() {
-	return killFlag;
-}
-
-float TeleporterSmoke::getPosX() {
-	return xPos;
-}
-
-float TeleporterSmoke::getYpos() {
-	return yPos;
 }
 
 char TeleporterSmoke::getDrawOrder() {
--- src/teleporterSmoke.hpp
+++ src/teleporterSmoke.hpp
@@ -13,30 +13,17 @@
 #include <stdio.h>
 #include "SFML/Graphics.hpp"
 #include "spriteSheet.hpp"
+#include "Effect.hpp"
 
-class TeleporterSmoke {
+class TeleporterSmoke : public Effect {
 private:
-	float xPos;
-	float yPos;
-	float xInit;
-	float yInit;
-    int32_t frameTimer;
-	uint8_t frameIndex;
 	SpriteSheet<16, 16> spriteSheet;
-   	bool killFlag;
-	// Smoke changes depth with time, store in variable
-	char drawOrder;
+   	char drawOrder;
 	
 public:
-	// Constructor arguments are initial position and input sprites from the effect controller object
-	TeleporterSmoke(sf::Texture *, float, float);
-	// For updating the position with external offset information
-	void update(float, float, sf::Time &);
-	const sf::Sprite & getSprite();
-	// Accessor function to determine whether the animation is done running
-	bool getKillFlag();
-	float getPosX();
-	float getYpos();
+   	TeleporterSmoke(sf::Texture *, float, float);
+	virtual void update(float, float, const sf::Time &);
+	virtual const sf::Sprite & getSprite();
 	char getDrawOrder();
 };
 #endif /* teleporterSmoke_hpp */
