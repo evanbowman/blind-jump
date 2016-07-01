a0a7e406cd29b9632b7bdc320e7738d3f1dfac31
evanbowman <ebowman@bu.edu>
2016-06-30 20:17:22 -0400

    Modified build scripts

---
+++ src/packageScriptMac.sh
@@ -0,0 +1,25 @@
+# arg2 is the app name, arg1 is the exe name
+if [ $# == 2 ]; then
+    rm -rf ../prod/$2.app/Contents/MacOS
+
+    mkdir -p ../prod/$2.app/Contents/MacOS
+    printf "\nCreated package directory"
+    
+    mv -f $1 ../prod/$2.app/Contents/MacOS/$1
+    printf "\nMoved executable to package"
+
+    printf "\nCopying resources into package..."
+    cp -rf ../Resources ../prod/$2.app/Contents/Resources
+
+    printf "\nCopying frameworks into package..."
+    cp -rf ../Frameworks ../prod/$2.app/Contents/Frameworks
+
+    if [ $? -eq 0 ]; then
+	printf "\nBundling complete!"
+    else
+	printf "\nFailed to create app bundle"
+    fi
+else
+    printf "\nUsage: packageScriptMac <executable> <name>"
+fi
+
