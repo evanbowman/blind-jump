f8bfa545b31f5558f6888e2d6b63f564bec1f073
evanbowman <ebowman@bu.edu>
2016-07-22 15:31:01 -0400

    Fixed compiler error

--- src/IntroDoor.cpp
+++ src/IntroDoor.cpp
@@ -22,50 +22,50 @@
 IntroDoor::IntroDoor(float _xInit, float _yInit, const sf::Texture & inpTxtr)
 	: Detail{_xInit, _yInit},
 	  doorSheet{inpTxtr},
-	  frameIndex{0}/
+	  frameIndex{0}/*,
 	  timer{0},
-	  state{State::dormant}
+	  state{State::dormant}*/
 {}
  
-bool checkBulletCollision(float xPos, float yPos, EffectGroup & ef) {
-	for (auto & element : ef.getBulletLayer1()) {
-		if (std::abs(element.getXpos() - xPos) < 4 && std::abs(element.getYpos() - yPos) < 4) {
-			element.setKillFlag();
-			return true;
-		}
-	}
-	return false;
-}
+// bool checkBulletCollision(float xPos, float yPos, EffectGroup & ef) {
+// 	for (auto & element : ef.getBulletLayer1()) {
+// 		if (std::abs(element.getXpos() - xPos) < 4 && std::abs(element.getYpos() - yPos) < 4) {
+// 			element.setKillFlag();
+// 			return true;
+// 		}
+// 	}
+// 	return false;
+// }
 
-void IntroDoor::_update(float xOffset, float yOffset, ScreenShakeController * pscr, EffectGroup & ef, const sf::Time & elapsedTime) {
-	switch (state) {
-		case State::dormant:
-		    if (checkBulletCollision(xOffset + xInit + 124, yPos + 62, ef)) {
-				state = State::opening;
-			}
-			break;
+// void IntroDoor::_update(float xOffset, float yOffset, ScreenShakeController * pscr, EffectGroup & ef, const sf::Time & elapsedTime) {
+// 	switch (state) {
+// 		case State::dormant:
+// 		    if (checkBulletCollision(xOffset + xInit + 124, yPos + 62, ef)) {
+// 				state = State::opening;
+// 			}
+// 			break;
 			
-		case State::opening:
-			timer += elapsedTime.asMilliseconds();
-			if (timer > 52) {
-				timer -= 52;
-				frameIndex++;
-				if (frameIndex > 3) {
-					frameIndex = 3;
-					state = State::opened;
-					pscr->rumble();
-				}
-			}
-			break;
+// 		case State::opening:
+// 			timer += elapsedTime.asMilliseconds();
+// 			if (timer > 52) {
+// 				timer -= 52;
+// 				frameIndex++;
+// 				if (frameIndex > 3) {
+// 					frameIndex = 3;
+// 					state = State::opened;
+// 					pscr->rumble();
+// 				}
+// 			}
+// 			break;
 			
-		case State::opened:
-			// Does nothing
-			break;
-	}
+// 		case State::opened:
+// 			// Does nothing
+// 			break;
+// 	}
     
-	yPos = yOffset + yInit;
-	doorSheet.setPosition(xOffset + xInit, yPos);
-}
+// 	yPos = yOffset + yInit;
+// 	doorSheet.setPosition(xOffset + xInit, yPos);
+// }
 
 const sf::Sprite & IntroDoor::getSprite() const {
 	return doorSheet[frameIndex];
