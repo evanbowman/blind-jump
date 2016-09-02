3e9beea74b56e044ed04ac70875e8e9c101ba0d5
evanbowman <ebowman@bu.edu>
2016-09-02 10:06:41 -0400

    Removed old target directory

--- src/packageScriptMac.sh
+++ src/packageScriptMac.sh
@@ -6,42 +6,42 @@
 
 # arg2 is the app name, arg1 is the exe name
 if [ $# == 2 ]; then
-    rm -rf ../target/$2.app
+    rm -rf ../targets/macOS/$2.app
 
-    mkdir -p ../target/$2.app/Contents/MacOS
+    mkdir -p ../targets/macOS/$2.app/Contents/MacOS
     printf "\nCreated parent package directory"
     
-    mv -f $1 ../target/$2.app/Contents/MacOS/$1
+    mv -f $1 ../targets/macOS/$2.app/Contents/MacOS/$1
     printf "\nMoved executable to package"
 
     printf "\nCopying resources into package... "
-    cp -rf ../Resources ../target/$2.app/Contents/
+    cp -rf ../Resources ../targets/macOS/$2.app/Contents/
     printf "Done!"
 
     printf "\nCopying frameworks into package... "
-    cp -rf ../Frameworks ../target/$2.app/Contents/
+    cp -rf ../Frameworks ../targets/macOS/$2.app/Contents/
     printf "Done!"
 
     printf "\nCopying sources into package... "
-    mkdir ../target/$2.app/Contents/src
-    cp -rf ./ ../target/$2.app/Contents/src
-    rm ../target/$2.app/Contents/src/*.o
+    mkdir ../targets/macOS/$2.app/Contents/src
+    cp -rf ./ ../targets/macOS/$2.app/Contents/src
+    rm ../targets/macOS/$2.app/Contents/src/*.o
     printf "Done!"
 
     printf "\nCompressing sources... "
-    tar -czf ../target/$2.app/Contents/src.tar.gz ../target/$2.app/Contents/src
-    rm -rf ../target/$2.app/Contents/src
+    tar -czf ../targets/macOS/$2.app/Contents/src.tar.gz ../targets/macOS/$2.app/Contents/src
+    rm -rf ../targets/macOS/$2.app/Contents/src
     printf "Done!"
     
     printf "\nMoving plist file... "
-    mv ../target/$2.app/Contents/Resources/info.plist ../target/$2.app/Contents/info.plist
+    mv ../targets/macOS/$2.app/Contents/Resources/info.plist ../targets/macOS/$2.app/Contents/info.plist
     printf "Done!"
     
     if [ $? -eq 0 ]; then
 	printf "\nBundling complete!"
     else
 	printf "\nFailed to create app bundle"
-	rm -rf ../target/$2.app/Contents/MacOS
+	rm -rf ../targets/macOS/$2.app/Contents/MacOS
     fi
 else
     printf "\nUsage: packageScriptMac <executable> <name>"
