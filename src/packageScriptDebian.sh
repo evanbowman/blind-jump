b358bf1926890e0b279d530b9885e7adf1700272
Evan Bowman <ebowman@bu.edu>
2016-10-14 17:46:07 -0400

    partial implementation of debian executables

--- src/packageScriptDebian.sh
+++ src/packageScriptDebian.sh
@@ -3,3 +3,16 @@
 # Liscensed under GPL 3, see: <http://www.gnu.org/licenses/>.            #
 #========================================================================#
 
+if [ $# == 2 ]; then
+    rm -rf ../targets/debian/$2.deb
+
+    mkdir -p ../targets/debian/$2/DEBIAN
+    mkdir -p ../targets/debian/$2/usr/bin
+    cp ../Resources/control ../targets/debian/$2/DEBIAN/control
+    cd ../targets/debian
+    dpkg-deb -b $2
+    rm -rf $2
+    cd ../../src
+else
+    printf "\nUsage: packageScriptDebian <executable> <name>"
+fi
