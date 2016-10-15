c1f97cb38823ce9e5433686b098aadb683e29db4
Evan Bowman <ebowman@bu.edu>
2016-10-15 16:46:20 -0400

    linux version of resourcePath() now relative from binary location

--- build/debian/ResourcePath.cpp
+++ build/debian/ResourcePath.cpp
@@ -1,5 +1,15 @@
 #include "../../src/ResourcePath.hpp"
+#include <unistd.h>
+#include <cstring>
 
 std::string resourcePath() {
-	return std::string("../Resources/");
+	static const size_t buffer_size = 512;
+	char buffer[buffer_size];
+	std::memset(buffer, 0, buffer_size);
+	[[gnu::unused]]
+	std::size_t bytesRead = readlink("/proc/self/exe", buffer, buffer_size);
+	const std::string path(buffer);
+	const std::size_t lastFwdSlash = path.find_last_of("/");
+    std::string pathWithoutBinary = path.substr(0, lastFwdSlash);
+	return pathWithoutBinary + "/../Resources/";
 }
